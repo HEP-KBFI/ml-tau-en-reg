82f5041bbc9ae826b1495d3211d38a430b8142da
Laurits <laurits.tani@gmail.com>
2024-04-23 12:10:20 +0300

    gen DV correction from .hepmc file

--- enreg/scripts/train-pytorch-gpu.sh
+++ enreg/scripts/train-pytorch-gpu.sh
@@ -6,5 +6,5 @@
 
 
 # apptainer exec -B /scratch/persistent/laurits --env PYTHONPATH=`pwd` --nv /home/software/singularity/pytorch.simg\:2024-02-13 python3 enreg/scripts/trainLorentzNet.py
-apptainer exec -B /scratch/persistent/laurits --env PYTHONPATH=`pwd` --nv /home/software/singularity/pytorch.simg\:2024-02-13 python3 enreg/scripts/trainSimpleDNN.py
+apptainer exec -B /home/laurits,/scratch/persistent/laurits --env PYTHONPATH=`pwd` --nv /home/software/singularity/pytorch.simg\:2024-02-13 python3 enreg/scripts/trainSimpleDNN.py
 # apptainer exec -B /scratch/persistent/laurits --env PYTHONPATH=`pwd` --nv /home/software/singularity/pytorch.simg\:2024-02-13 python3 enreg/scripts/trainParticleTransformer.py
