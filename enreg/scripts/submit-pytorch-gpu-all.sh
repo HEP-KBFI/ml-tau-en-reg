1f04c9f9cbab0dd6170cb467ab67984166c313ee
Joosep Pata <joosep.pata@cern.ch>
2024-05-17 16:00:17 +0300

    better reproducibility

--- enreg/scripts/submit-pytorch-gpu-all.sh
+++ enreg/scripts/submit-pytorch-gpu-all.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
+#edit this as needed
+export OUTDIR=training-outputs/240517_fullstats/
+
 #for regression and decay mode, use only signal (tau) jets
 export TRAIN_SAMPS=zh_train.parquet
 export TEST_SAMPS=z_test.parquet,zh_test.parquet
-sbatch enreg/scripts/train-pytorch-gpu-full.sh jet_regression LorentzNet $TRAIN_SAMPS $TEST_SAMPS
-sbatch enreg/scripts/train-pytorch-gpu-full.sh jet_regression ParticleTransformer $TRAIN_SAMPS $TEST_SAMPS
-sbatch enreg/scripts/train-pytorch-gpu-full.sh jet_regression SimpleDNN $TRAIN_SAMPS $TEST_SAMPS
+sbatch enreg/scripts/train-pytorch-gpu-full.sh output_dir=OUTDIR training_samples=[$TRAIN_SAMPS] test_samples=[$TEST_SAMPS] training_type=jet_regression model_type=LorentzNet
+sbatch enreg/scripts/train-pytorch-gpu-full.sh output_dir=OUTDIR training_samples=[$TRAIN_SAMPS] test_samples=[$TEST_SAMPS] training_type=jet_regression model_type=ParticleTransformer
+sbatch enreg/scripts/train-pytorch-gpu-full.sh output_dir=OUTDIR training_samples=[$TRAIN_SAMPS] test_samples=[$TEST_SAMPS] training_type=jet_regression model_type=SimpleDNN
 
-sbatch enreg/scripts/train-pytorch-gpu-full.sh dm_multiclass LorentzNet $TRAIN_SAMPS $TEST_SAMPS
-sbatch enreg/scripts/train-pytorch-gpu-full.sh dm_multiclass ParticleTransformer $TRAIN_SAMPS $TEST_SAMPS
-sbatch enreg/scripts/train-pytorch-gpu-full.sh dm_multiclass SimpleDNN $TRAIN_SAMPS $TEST_SAMPS
+sbatch enreg/scripts/train-pytorch-gpu-full.sh output_dir=OUTDIR training_samples=[$TRAIN_SAMPS] test_samples=[$TEST_SAMPS] training_type=dm_multiclass model_type=LorentzNet
+sbatch enreg/scripts/train-pytorch-gpu-full.sh output_dir=OUTDIR training_samples=[$TRAIN_SAMPS] test_samples=[$TEST_SAMPS] training_type=dm_multiclass model_type=ParticleTransformer
+sbatch enreg/scripts/train-pytorch-gpu-full.sh output_dir=OUTDIR training_samples=[$TRAIN_SAMPS] test_samples=[$TEST_SAMPS] training_type=dm_multiclass model_type=SimpleDNN
 
 ##for binary classification, use signal (tau) and background (non-tau) jets
 export TRAIN_SAMPS=zh_train.parquet,qq_train.parquet
 export TEST_SAMPS=z_test.parquet,zh_test.parquet,qq_test.parquet
-sbatch enreg/scripts/train-pytorch-gpu-full.sh binary_classification LorentzNet $TRAIN_SAMPS $TEST_SAMPS
-sbatch enreg/scripts/train-pytorch-gpu-full.sh binary_classification ParticleTransformer $TRAIN_SAMPS $TEST_SAMPS
-sbatch enreg/scripts/train-pytorch-gpu-full.sh binary_classification SimpleDNN $TRAIN_SAMPS $TEST_SAMPS
+sbatch enreg/scripts/train-pytorch-gpu-full.sh output_dir=OUTDIR training_samples=[$TRAIN_SAMPS] test_samples=[$TEST_SAMPS] training_type=binary_classification model_type=LorentzNet
+sbatch enreg/scripts/train-pytorch-gpu-full.sh output_dir=OUTDIR training_samples=[$TRAIN_SAMPS] test_samples=[$TEST_SAMPS] training_type=binary_classification model_type=ParticleTransformer
+sbatch enreg/scripts/train-pytorch-gpu-full.sh output_dir=OUTDIR training_samples=[$TRAIN_SAMPS] test_samples=[$TEST_SAMPS] training_type=binary_classification model_type=SimpleDNN
--- enreg/scripts/train-pytorch-gpu-full.sh
+++ enreg/scripts/train-pytorch-gpu-full.sh
@@ -10,10 +10,6 @@ MODEL_TYPE=$2
 TRAIN_SAMPS=$3
 TEST_SAMPS=$4
 
-#keras is not used, but for some reason, it's imported somewhere and crashes if this is not specified
 export KERAS_BACKEND=torch
 
-#on manivald
-export RUNCMD="singularity exec -B /scratch/persistent --env PYTHONPATH=`pwd` --nv /home/software/singularity/pytorch.simg:2024-04-30 "
-
-$RUNCMD python3 enreg/scripts/trainModel.py training_type=$TRAINING_TYPE model_type=$MODEL_TYPE training_samples=[$TRAIN_SAMPS] test_samples=[$TEST_SAMPS]
+./run.sh python3 enreg/scripts/trainModel.py "@$"
