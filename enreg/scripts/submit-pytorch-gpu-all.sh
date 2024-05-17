f4ce45647595052232442fce6b71bfc704b90406
Joosep Pata <joosep.pata@cern.ch>
2024-05-17 09:53:44 +0300

    up

--- enreg/scripts/submit-pytorch-gpu-all.sh
+++ enreg/scripts/submit-pytorch-gpu-all.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
-export OUTPUT_DIR=/home/$USER/ml-tau-en-reg/training-outputs/240516_fullstats/
-export DATA_PATH=/scratch/persistent/joosep/ml-tau/20240402_full_stats_merged
-
 #for regression and decay mode, use only signal (tau) jets
 export TRAIN_SAMPS=zh_train.parquet
 export TEST_SAMPS=z_test.parquet,zh_test.parquet
-sbatch enreg/scripts/train-pytorch-gpu-full.sh jet_regression LorentzNet
-sbatch enreg/scripts/train-pytorch-gpu-full.sh jet_regression ParticleTransformer
-sbatch enreg/scripts/train-pytorch-gpu-full.sh jet_regression SimpleDNN
+sbatch enreg/scripts/train-pytorch-gpu-full.sh jet_regression LorentzNet $TRAIN_SAMPS $TEST_SAMPS
+sbatch enreg/scripts/train-pytorch-gpu-full.sh jet_regression ParticleTransformer $TRAIN_SAMPS $TEST_SAMPS
+sbatch enreg/scripts/train-pytorch-gpu-full.sh jet_regression SimpleDNN $TRAIN_SAMPS $TEST_SAMPS
 
-sbatch enreg/scripts/train-pytorch-gpu-full.sh dm_multiclass LorentzNet
-sbatch enreg/scripts/train-pytorch-gpu-full.sh dm_multiclass ParticleTransformer
-sbatch enreg/scripts/train-pytorch-gpu-full.sh dm_multiclass SimpleDNN
+sbatch enreg/scripts/train-pytorch-gpu-full.sh dm_multiclass LorentzNet $TRAIN_SAMPS $TEST_SAMPS
+sbatch enreg/scripts/train-pytorch-gpu-full.sh dm_multiclass ParticleTransformer $TRAIN_SAMPS $TEST_SAMPS
+sbatch enreg/scripts/train-pytorch-gpu-full.sh dm_multiclass SimpleDNN $TRAIN_SAMPS $TEST_SAMPS
 
-#for binary classification, use signal (tau) and background (non-tau) jets
+##for binary classification, use signal (tau) and background (non-tau) jets
 export TRAIN_SAMPS=zh_train.parquet,qq_train.parquet
 export TEST_SAMPS=z_test.parquet,zh_test.parquet,qq_test.parquet
-sbatch enreg/scripts/train-pytorch-gpu-full.sh binary_classification LorentzNet
-sbatch enreg/scripts/train-pytorch-gpu-full.sh binary_classification ParticleTransformer
-sbatch enreg/scripts/train-pytorch-gpu-full.sh binary_classification SimpleDNN
+sbatch enreg/scripts/train-pytorch-gpu-full.sh binary_classification LorentzNet $TRAIN_SAMPS $TEST_SAMPS
+sbatch enreg/scripts/train-pytorch-gpu-full.sh binary_classification ParticleTransformer $TRAIN_SAMPS $TEST_SAMPS
+sbatch enreg/scripts/train-pytorch-gpu-full.sh binary_classification SimpleDNN $TRAIN_SAMPS $TEST_SAMPS
