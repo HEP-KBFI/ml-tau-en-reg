4941cc9b4bfc81afb8bd3fec0a97783a5c9681e6
Joosep Pata <joosep.pata@cern.ch>
2024-05-18 08:19:22 +0300

    fix

--- enreg/scripts/submit-pytorch-gpu-all.sh
+++ enreg/scripts/submit-pytorch-gpu-all.sh
@@ -17,6 +17,6 @@ sbatch enreg/scripts/train-pytorch-gpu.sh output_dir=$OUTDIR training_samples=[$
 ##for binary classification, use signal (tau) and background (non-tau) jets
 export TRAIN_SAMPS=zh_train.parquet,qq_train.parquet
 export TEST_SAMPS=z_test.parquet,zh_test.parquet,qq_test.parquet
-sbatch enreg/scripts/train-pytorch-gpu.sh output_dir=OUTDIR training_samples=[$TRAIN_SAMPS] test_samples=[$TEST_SAMPS] training_type=binary_classification model_type=LorentzNet
-sbatch enreg/scripts/train-pytorch-gpu.sh output_dir=OUTDIR training_samples=[$TRAIN_SAMPS] test_samples=[$TEST_SAMPS] training_type=binary_classification model_type=ParticleTransformer
-sbatch enreg/scripts/train-pytorch-gpu.sh output_dir=OUTDIR training_samples=[$TRAIN_SAMPS] test_samples=[$TEST_SAMPS] training_type=binary_classification model_type=SimpleDNN
+sbatch enreg/scripts/train-pytorch-gpu.sh output_dir=$OUTDIR training_samples=[$TRAIN_SAMPS] test_samples=[$TEST_SAMPS] training_type=binary_classification model_type=LorentzNet
+sbatch enreg/scripts/train-pytorch-gpu.sh output_dir=$OUTDIR training_samples=[$TRAIN_SAMPS] test_samples=[$TEST_SAMPS] training_type=binary_classification model_type=ParticleTransformer
+sbatch enreg/scripts/train-pytorch-gpu.sh output_dir=$OUTDIR training_samples=[$TRAIN_SAMPS] test_samples=[$TEST_SAMPS] training_type=binary_classification model_type=SimpleDNN
