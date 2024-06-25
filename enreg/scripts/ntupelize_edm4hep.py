94de203ca293ca5a506f115e42caf1d778d780ed
Joosep Pata <joosep.pata@cern.ch>
2024-06-25 11:25:24 +0300

    produce merged ntuples

--- enreg/scripts/ntupelize_edm4hep.py
+++ enreg/scripts/ntupelize_edm4hep.py
@@ -4,76 +4,99 @@ import glob
 import hydra
 import awkward as ak
 import multiprocessing
+import numpy as np
 from itertools import repeat
 from omegaconf import DictConfig
 from enreg.tools.data_management import ntupelizer as nt
 from enreg.tools.data_management import ntupelizer_slurm_tools as nst
 
 
-def save_record_to_file(data: dict, output_path: str) -> None:
-    print(f"Saving to precessed data to {output_path}")
+def save_record_to_file(data: ak.Record, output_path: str) -> None:
+    print(f"Saving to {len(data)} processed entries to {output_path}")
     ak.to_parquet(data, output_path)
 
 
-def process_single_file(
-    input_path: str,
+#process a list of input files, merge into a single output file
+def process_and_merge(
+    input_paths: list[str],
     output_path: str,
     cfg: DictConfig
 ):
     sample = os.path.basename(os.path.dirname(output_path))
+    datas = []
     if not os.path.exists(output_path):
         start_time = time.time()
         remove_bkg = cfg.samples[sample].is_signal
-        data = nt.process_input_file(input_path, cfg.tree_path, cfg.branches, remove_background=remove_bkg)
-        save_record_to_file(data, output_path)
-        end_time = time.time()
-        print(f"Finished processing in {end_time-start_time} s.")
-    else:
-        print("File already processed, skipping.")
+        for input_path in input_paths:
+            print("processing {}".format(input_path))
+            data = nt.process_input_file(input_path, cfg.tree_path, cfg.branches, remove_background=remove_bkg)
+            #Record -> Array to allow concat later on
+            data = ak.Array({k: data[k] for k in data.fields}) 
+            datas.append(data)
 
+        #merge outputs from all inputs
+        data = ak.concatenate(datas)
+        save_record_to_file(data, output_path)
+
+        end_time = time.time()
+        print(f"Finished processing in {end_time-start_time:.2f} s.")
+    else:
+        print("File {} already processed, skipping.".format(output_path))
+
+
+#actually run the ntuplizer on slurm
+def run_job(cfg: DictConfig):
+    input_paths = []
+    with open(cfg.input_file, 'rt') as inFile:
+        for line in inFile:
+            input_paths.append(line.strip('\n'))
+    output_paths = []
+    with open(cfg.output_file, 'rt') as inFile:
+        for line in inFile:
+            output_paths.append(line.strip('\n'))
+
+    #must have exactly one output file
+    assert(len(output_paths) == 1)
+    output_path = output_paths[0]
+
+    output_dir = os.path.dirname(output_path)
+    os.makedirs(output_dir, exist_ok=True)
+    process_and_merge(input_paths, output_path, cfg)
+
+
+#input preparation on interactive node
+def prepare_inputs(cfg: DictConfig):
+    all_input_paths = []
+    all_output_paths = []
+
+    for sample_name in cfg.samples_to_process:
+        print(f"creating inputs for {sample_name}")
+        output_dir = cfg.samples[sample_name].output_dir
+        input_dir = cfg.samples[sample_name].input_dir
+        os.makedirs(output_dir, exist_ok=True)
+        input_wcp = os.path.join(input_dir, "*.root")
+        
+        #divide the input list into chunks of files_per_job
+        #each chunk of N input files will yield exactly one output file 
+        input_paths = sorted(list(glob.glob(input_wcp)[:cfg.n_files]))
+        input_path_chunks = list(np.array_split(input_paths, len(input_paths)//cfg.files_per_job))
+        print(f"found {len(input_paths)} files, {len(input_path_chunks)} chunks")
+        output_paths = [os.path.join(output_dir, os.path.basename(chunk[0]).replace(".root", ".parquet")) for chunk in input_path_chunks]
+
+        all_output_paths.extend(output_paths)
+        all_input_paths.extend(input_path_chunks)
+
+    #create executables for slurm
+    nst.multipath_slurm_ntupelizer(all_input_paths, all_output_paths)
 
 @hydra.main(config_path="../config", config_name="ntupelizer", version_base=None)
-def process_all_input_files(cfg: DictConfig) -> None:
+def main(cfg: DictConfig) -> None:
     print("Working directory : {}".format(os.getcwd()))
-    if cfg.slurm_run:
-        input_paths = []
-        with open(cfg.input_file, 'rt') as inFile:
-            for line in inFile:
-                input_paths.append(line.strip('\n'))
-        output_paths = []
-        with open(cfg.output_file, 'rt') as inFile:
-            for line in inFile:
-                output_paths.append(line.strip('\n'))
-        for input_path, output_path in zip(input_paths, output_paths):
-            output_dir = os.path.dirname(output_path)
-            os.makedirs(output_dir, exist_ok=True)
-            process_single_file(input_path, output_path, cfg)
-    else:
-        all_input_paths = []
-        all_output_paths = []
-        for sample_name in cfg.samples_to_process:
-            output_dir = cfg.samples[sample_name].output_dir
-            input_dir = cfg.samples[sample_name].input_dir
-            os.makedirs(output_dir, exist_ok=True)
-            input_wcp = os.path.join(input_dir, "*.root")
-            if cfg.test_run:
-                n_files = 3
-            else:
-                n_files = None
-            input_paths = glob.glob(input_wcp)[:cfg.n_files]
-            file_names = [os.path.basename(input_path).replace(".root", ".parquet") for input_path in input_paths]
-            output_paths = [os.path.join(output_dir, file_name) for file_name in file_names]
-            all_output_paths.extend(output_paths)
-            all_input_paths.extend(input_paths)
-        if cfg.use_multiprocessing:
-            pool = multiprocessing.Pool(processes=10)
-            pool.starmap(process_single_file, zip(all_input_paths, all_output_paths, repeat(cfg)))
-        elif cfg.use_slurm:
-            nst.multipath_slurm_ntupelizer(all_input_paths, all_output_paths, batch_size=20)
-        else:
-            for input_path, output_path in zip(all_input_paths, all_output_paths):
-                process_single_file(input_path, output_path, cfg)
 
+    if cfg.slurm_run:
+        run_job(cfg)
+    else:
+        prepare_inputs(cfg)
 
 if __name__ == "__main__":
-    process_all_input_files()
+    main()
--- sim/README.md
+++
@@ -1,4 +0,0 @@
-Edit `run_sim.sh` to change the output directory, then generate 100 events:
-```
-./run_sim.sh 1 p8_ee_ZH_Htautau_ecm380.cmd 
-```
