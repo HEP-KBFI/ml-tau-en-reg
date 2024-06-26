af6b4832ca657c8fc86b5160e69a0e21aa54bf83
Joosep Pata <joosep.pata@cern.ch>
2024-06-26 12:57:47 +0300

    use more recent image

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 #keras is not used, but for some reason, it's imported somewhere and crashes if this is not specified
 export KERAS_BACKEND=torch
-apptainer exec -B /home/laurits,/scratch/persistent,/local/joosep --env PYTHONPATH=`pwd` --nv /home/software/singularity/pytorch.simg\:2024-04-30 "$@"
+apptainer exec -B /scratch/persistent,/local --env PYTHONPATH=`pwd` --nv /home/software/singularity/pytorch.simg\:2024-05-21 "$@"
