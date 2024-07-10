ce324594f2fa8ee83fa7a00d7d695f7e30d6a054
Joosep Pata <joosep.pata@gmail.com>
2024-07-10 15:31:22 +0300

    dataloader iterative

--- enreg/scripts/trainModel.py
+++ enreg/scripts/trainModel.py
@@ -34,7 +34,7 @@ from enreg.tools.models.LorentzNet import LorentzNet
 
 from enreg.tools.data_management.features import FeatureStandardization
 
-from enreg.tools.data_management.particleTransformer_dataset import ParticleTransformerDataset
+from enreg.tools.data_management.particleTransformer_dataset import load_row_groups, ParticleTransformerDataset
 
 from enreg.tools.models.logTrainingProgress import logTrainingProgress, logTrainingProgress_regression
 from enreg.tools.models.logTrainingProgress import logTrainingProgress_decaymode
@@ -249,7 +249,6 @@ def trainModel(cfg: DictConfig) -> None:
 
     kind = cfg.training_type
     model_config = cfg.models[cfg.model_type]
-    DatasetClass = ParticleTransformerDataset
 
     model_output_path = os.path.join(
         cfg.output_dir,
@@ -263,25 +262,26 @@ def trainModel(cfg: DictConfig) -> None:
     if cfg.train and os.path.isdir(model_output_path):
         raise Exception("Output directory exists while train=True: {}".format(model_output_path))
 
-    data = g.load_all_data([os.path.join(cfg.data_path, samp) for samp in cfg.training_samples])
+    # get the number of row groups (independently loadable chunks) in each input file
+    data = sum([load_row_groups(os.path.join(cfg.data_path, fn)) for fn in cfg.training_samples], [])
 
-    #shuffle input samples
+    #shuffle row groups
     perm = np.random.permutation(len(data))
-    data_reshuf = data[perm]
+    data_reshuf = [data[p] for p in perm]
 
-    #take train and validation split
+    #take train and validation split from the row groups
     ntrain = int(len(data_reshuf)*cfg.fraction_train)
     nvalid = int(len(data_reshuf)*cfg.fraction_valid)
     training_data = data_reshuf[:ntrain]
     validation_data = data_reshuf[ntrain:ntrain+nvalid]
-    print("train={} validation={}".format(len(training_data), len(validation_data)))
+    print(f"row groups: train={len(training_data)} validation={len(validation_data)}")
     
-    dataset_train = DatasetClass(
-        data=training_data,
+    dataset_train = ParticleTransformerDataset(
+        row_groups=training_data,
         cfg=cfg.dataset,
     )
-    dataset_validation = DatasetClass(
-        data=validation_data,
+    dataset_validation = ParticleTransformerDataset(
+        row_groups=validation_data,
         cfg=cfg.dataset,
     )
     if kind == "jet_regression" and cfg.training.apply_regression_weights:
@@ -290,10 +290,11 @@ def trainModel(cfg: DictConfig) -> None:
         dataset_validation.weight_tensors = weights_validation
 
     dev = "cuda" if torch.cuda.is_available() else "cpu"
-    print("Using device: {}".format(dev))
+    print(f"Using device: {dev}")
 
     print("Building model...")
 
+    #configure the number of model input dimensions based on the input features 
     input_dim = 0
     if 'cand_kinematics' in feature_set:
         input_dim += 4
@@ -325,7 +326,7 @@ def trainModel(cfg: DictConfig) -> None:
             c_weight=cfg.models.LorentzNet.hyperparameters.c_weight,
             verbosity=cfg.verbosity,
         ).to(device=dev)
-    elif cfg.model_type == "SimpleDNN": # Input dim changes 
+    elif cfg.model_type == "SimpleDNN":
         model = DeepSet(input_dim, num_classes).to(device=dev)
 
     initWeights(model)
@@ -342,15 +343,13 @@ def trainModel(cfg: DictConfig) -> None:
             dataset_train,
             batch_size=cfg.training.batch_size,
             num_workers=cfg.training.num_dataloader_workers,
-            prefetch_factor=10,
-            shuffle=True
+            prefetch_factor=cfg.training.prefetch_factor,
         )
         dataloader_validation = DataLoader(
             dataset_validation,
             batch_size=cfg.training.batch_size,
             num_workers=cfg.training.num_dataloader_workers,
-            prefetch_factor=10,
-            shuffle=True
+            prefetch_factor=cfg.training.prefetch_factor,
         )
 
         if kind == "binary_classification":
@@ -470,18 +469,16 @@ def trainModel(cfg: DictConfig) -> None:
         print("Evaluating on test samples")
         for test_sample in cfg.test_samples:
             print("Evaluating on {}".format(test_sample))
-            data = g.load_all_data([str(os.path.join(cfg.data_path, test_sample))])
-            dataset_full = DatasetClass(
-                data=data,
+            data = load_row_groups(os.path.join(cfg.data_path, test_sample))
+            dataset_full = ParticleTransformerDataset(
+                row_groups=data,
                 cfg=cfg.dataset,
-                do_preselection=False #for evaluation, ensure we do no selection
             )
             dataloader_full = DataLoader(
                 dataset_full,
                 batch_size=cfg.training.batch_size,
                 num_workers=cfg.training.num_dataloader_workers,
-                prefetch_factor=100,
-                shuffle=False
+                prefetch_factor=cfg.training.prefetch_factor,
             )
             preds = []
             targets = []
