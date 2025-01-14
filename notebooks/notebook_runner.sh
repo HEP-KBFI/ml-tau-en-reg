46a4d537467f0498c8e79f460fbfdbd051eb76a9
HardiVv <hardiveski@gmail.com>
2025-01-14 18:27:41 +0200

    wrestling with directories

--- notebooks/notebook_runner.sh
+++ notebooks/notebook_runner.sh
@@ -2,4 +2,5 @@
 
 echo "Running master evalutator notebook with parameters from evaluation_params.yaml"
 
-../run.sh papermill master_evaluator.ipynb master_evaluator.ipynb -f evaluation_params.yaml
+#./run.sh papermill master_evaluator.ipynb master_evaluator.ipynb -f evaluation_params.yaml
+papermill notebooks/master_evaluator.ipynb notebooks/master_evaluator.ipynb -f enreg/config/evaluation_params.yaml
