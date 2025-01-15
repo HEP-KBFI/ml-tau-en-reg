34768c8f305c335904b8cc563655fae8d8435ff9
HardiVv <hardiveski@gmail.com>
2025-01-15 17:22:33 +0200

    up

--- notebooks/notebook_runner.sh
+++ notebooks/notebook_runner.sh
@@ -3,4 +3,4 @@
 echo "Running master evalutator notebook with parameters from evaluation_params.yaml"
 
 #./run.sh papermill master_evaluator.ipynb master_evaluator.ipynb -f evaluation_params.yaml
-papermill notebooks/master_evaluator.ipynb notebooks/master_evaluator.ipynb -f enreg/config/evaluation_params.yaml
+papermill notebooks/master_evaluator.ipynb notebooks/master_evaluator_papermill.ipynb -f enreg/config/evaluation_params.yaml
